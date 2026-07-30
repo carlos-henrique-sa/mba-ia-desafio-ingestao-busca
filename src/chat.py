8644fe70a38716bac92b24721111f7a795571e80
carlos-henrique-sa <carlos.henrique.sa.filho@gmail.com>
2026-07-30 20:46:18 -0300

    prep models

--- src/chat.py
+++ src/chat.py
@@ -1,4 +1,25 @@
+from langchain.chat_models import init_chat_model
+from langchain_openai import ChatOpenAI
 from search import search_prompt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+model_openai = ChatOpenAI(model=os.getenv("OPENAI_CHAT_MODEL", "bzl/gpt-5.4-nano"), temperature=0.5)
+
+message_openai = model_openai.invoke("hello world openai")
+print(message_openai.content)
+
+
+model = init_chat_model(
+    model=os.getenv("GOOGLE_CHAT_MODEL", "gemini-2.5-flash"),
+    model_provider="google_genai",
+    temperature=0.5,
+)
+
+answer_gemini = model.invoke("hello world gemini")
+print(answer_gemini.content)
 
 def main():
     chain = search_prompt()
